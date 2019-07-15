84693942b43fd608e8305f5fc376293c13fd2862
Joel Mendoza <joelmendza173@gmail.com>
2019-07-15 00:22:30 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, element)
+  array.push(element)
+end   
+
+def using_unshift(array, element)
+  array.unshift(element)
+end 
+
+def using_pop(array)
+  array.pop
+end 
+
+def using_pop(array)
+  array.pop(2)
+end 
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end 
+
+def using_insert(array, element)
+  array.insert(4, element)
+end  
+
+def using_uniq(array)
+  array.uniq
+end 
